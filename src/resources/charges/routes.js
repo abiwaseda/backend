f151d45058fb9bd5ca9e9ddeff1e8d031a72a9d9
nash90 <nash.rakuten@gmail.com>
2016-12-10 19:10:43 +0900

    created api for stripe capture and authorization

--- src/resources/charges/routes.js
+++ src/resources/charges/routes.js
@@ -1,21 +1,14 @@
 // API endpoint handlers
 import {
-    ChargeHandler
+    StripeChargeHandler
 } from './handlers';
 
 export default [
-    {
-        path: '',
-        method: 'GET',
-        config: {
-            handler: {async: ChargeHandler.get}
-        }
-    },
 	{
-        path: '',
+        path: '/stripe',
         method: 'POST',
         config: {
-            handler: {async: ChargeHandler.post}
+            handler: {async: StripeChargeHandler.post}
         }
     }
 ]
